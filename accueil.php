da9ba2b63412f462e3ae2cde7b55f46e1cff73cc
unknown <sio1@c106-16.220056s.local>
2025-11-21 17:23:20 +0100

    caroussel

---
+++ accueil.php
@@ -0,0 +1,50 @@
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+     </head>
+     <body>
+         <div class="container-fluid">
+
+  <div class="row">
+
+    <div class="col-md-8">
+        <p>La bibliothèque de Mouinsort est fermée au public jusqu'à nouvel ordre Mais il vous est possible de réserver et retirer vos livres via notre service Biblio Drive !</p>
+        
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <form class="form-inline" action="/action_page.php">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-success" type="submit">Search</button>
+            </form>
+        </nav>              
+    
+
+    </div>
+
+   <div class="col-md-4">
+
+    
+ 
+
+    </div>
+
+
+
+  </div>
+
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+     </body>
+     </html>
